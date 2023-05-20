37df3c71413c265d22df5754bd2f183b4d0517fc
TaehyeonPark <devtaehyeon@gmail.com>
2023-05-21 02:39:07 +0900

    noti: this will be backup commit

--- ldap.py
+++ ldap.py
@@ -1,5 +1,127 @@
-from ldap3 import Server, Connection, ALL, NTLM
+import subprocess
+import ldap3
 
-def get_ldap_connection(nickname : str = None):
-    if nickname == None:
-        return None
+adminPassword = "admin_password"
+ldapHost = 'ldap.sparcs.org'
+
+shell = '/bin/sh'
+uid = lambda: int(subprocess.check_output(['id', '-u', 'nobody']).decode().strip())
+opt = {'shell': shell, 'uid': uid}
+
+def escape(str):
+    return str.replace('\\', '\\\\').replace('"', '\\"').replace('$', '\\$')
+
+host = ['-H', ldapHost]
+admin = ['-D', 'cn=admin,dc=sparcs,dc=org']
+def dnOnly(un):
+    return ['uid=' + escape(un) + ',ou=People,dc=sparcs,dc=org']
+def dn(un):
+    return ['-D'] + dnOnly(un)
+def password(pw):
+    return ['-w', escape(pw)]
+def newPassword(pw):
+    return ['-s', escape(pw)]
+def file(path):
+    return ['-f', escape(path)]
+
+adminPass = password(adminPassword)
+searchSuffix = ['-x', '-LLL', '-b', 'ou=People,dc=sparcs,dc=org', '|', 'grep', 'uidNumber:']
+
+def e(params):
+    return subprocess.run(params, **opt, capture_output=True, text=True)
+
+async def auth(un, pw):
+    command = 'ldapwhoami'
+    if un and pw:
+        result = await e([command] + host + dn(un) + password(pw))
+        if result.returncode != 0 or len(result.stdout) == 0 or len(result.stderr) != 0:
+            raise Exception({'command': command, 'err': result.returncode, 'stdout': result.stdout, 'stderr': result.stderr})
+    else:
+        raise Exception({'command': command, 'un': un, 'pw': pw})
+
+async def passwd(un, opass, npass):
+    command = 'ldappasswd'
+    if un and opass and npass:
+        result = await e([command] + host + dn(un) + password(opass) + newPassword(npass) + ['-S'])
+        if result.returncode != 0 or len(result.stdout) != 0 or len(result.stderr) != 0:
+            raise Exception({'command': command, 'err': result.returncode, 'stdout': result.stdout, 'stderr': result.stderr})
+    else:
+        raise Exception({'command': command, 'un': un, 'opass': opass, 'npass': npass})
+
+async def passwdByAdmin(un, npass):
+    command = 'ldappasswd'
+    if un and npass:
+        result = await e([command] + host + admin + adminPass + newPassword(npass) + ['-S'] + dnOnly(un))
+        if result.returncode != 0 or len(result.stdout) != 0 or len(result.stderr) != 0:
+            raise Exception({'command': command, 'err': result.returncode, 'stdout': result.stdout, 'stderr': result.stderr})
+    else:
+        raise Exception({'command': command, 'un': un, 'npass': npass})
+
+async def uids():
+    command = 'ldapsearch'
+    result = await e([command] + host + admin + adminPass + searchSuffix)
+    if result.returncode != 0 or len(result.stderr) != 0:
+        raise Exception({'command': command, 'err': result.returncode, 'stdout': result.stdout, 'stderr': result.stderr})
+    else:
+        return [int(s.strip()) for s in result.stdout.replace('uidNumber: ', '').split('\n')]
+
+def add(path):
+    command = 'ldapadd'
+    if path:
+        return e([command, host, admin, adminPass, file(path)]).then(lambda result: None).catch(lambda err: {'command': command, 'err': err})
+    else:
+        return {'command': command, 'path': None}
+
+def delete(un):
+    command = 'ldapdelete'
+    if un:
+        return e([command, host, admin, adminPass, dnOnly(un)]).then(lambda result: None).catch(lambda err: {'command': command, 'err': err})
+    else:
+        return {'command': command, 'un': None}
+
+def ldif(un, uid):
+  return '\n'.join([
+    f"dn: uid={un},ou=People,dc=sparcs,dc=org",
+    f"uid: {un}",
+    f"cn: {un}",
+    'objectClass: account',
+    'objectClass: posixAccount',
+    'objectClass: top',
+    'objectClass: shadowAccount',
+    'objectClass: postfixAccount',
+    'shadowMax: 99999',
+    'shadowWarning: 7',
+    'loginShell: /bin/bash',
+    f"uidNumber: {uid}",
+    'gidNumber: 400',
+    f"homeDirectory: /home/{un}",
+    f"mail: {un}@sparcs.org"
+  ])
+
+def bind(un, pw):
+    dn = f'uid={un},ou=People,dc=sparcs,dc=org'
+    try:
+        server = ldap3.Server(host=ldapHost, get_info=ldap3.ALL)
+        conn = ldap3.Connection(server, dn, pw, auto_bind=True)
+        return True
+    except ldap3.core.exceptions.LDAPBindError as e:
+        print(e)
+        return False
+
+if __name__ == "__main__":
+    from getpass import getpass
+    address = 'ldap.sparcs.org'
+    un = input('Username: ')
+    pw = getpass('Password: ')
+    dn = f'uid={un},ou=People,dc=sparcs,dc=org'
+    server = ldap3.Server(address, get_info=ldap3.ALL)
+    conn = None
+    try:
+        conn = ldap3.Connection(server, dn, pw, auto_bind=True)
+    except ldap3.core.exceptions.LDAPBindError as e:
+        print(e)
+        exit(1)
+    
+    conn.search('ou=People,dc=sparcs,dc=org', '(objectclass=posixAccount)', attributes=['uidNumber'])
+
+    print(conn.entries[0]['uidNumber'])
--- route.py
+++ route.py
@@ -1,6 +1,8 @@
 import fastapi
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi import Depends
 
 from uvicorn import run
 from typing import List, Dict, Any, Union, Optional
@@ -8,8 +10,11 @@ from typing import List, Dict, Any, Union, Optional
 from sqlalchemy.orm import Session
 from redis import Redis
 
+from json import loads
+
 from database import SessionLocal, engine, get_db
-import crud, models, schema, crud_v2
+import models, schema, crud_v2
+import ldap
 
 app = fastapi.FastAPI()
 
@@ -19,15 +24,15 @@ async def root():
     return {"status": 200, "message": "Welcome to NUGU API Server", "data": None}
 
 @app.get("/api/v1/help")
-async def example_for_dummy_data(data: Optional[schema.HELP] = None, request: fastapi.Request = None):
+async def example_for_dummy_data(data: Optional[schema.HELP] = None, request: Request = None):
     return Jinja2Templates(directory="templates").TemplateResponse("help.html", context={"request": request})
 
 @app.get("/api/v2/help")
-async def example_for_dummy_data(data: Optional[schema.HELP] = None, request: fastapi.Request = None):
+async def example_for_dummy_data(data: Optional[schema.HELP] = None, request: Request = None):
     return Jinja2Templates(directory="templates").TemplateResponse("help.html", context={"request": request})
 
 @app.post("/api/v2/insert/{table}")
-async def insert(table: str, data: Dict[str, Any], db: Session = fastapi.Depends(get_db)):
+async def insert(table: str, data: Dict[str, Any], db: Session = Depends(get_db)):
     if table not in models.TABLES:
         return {"detail": f"{table} is not in schema"}
     if data['nickname'] == None and table != None and data != None:
@@ -35,22 +40,46 @@ async def insert(table: str, data: Dict[str, Any], db: Session = fastapi.Depends
     return crud_v2.insert(db, table, data)
 
 @app.post("/api/v2/update/{table}")
-async def update(table: str, data: Dict[str, Any], db: Session = fastapi.Depends(get_db)):
+async def update(table: str, data: Dict[str, Any], db: Session = Depends(get_db)):
     if table not in models.TABLES:
         return {"detail": f"{table} is not in schema"}
     if data['nickname'] == None and table != None and data != None:
         return {"status": 400, "message": f"REQ => Fail to update {table} | Bad Request", "data": None}
     return crud_v2.update(db, table, data)
 
-@app.post("/api/v2/read/{table}")
-async def read(table: str, data: schema.READ, db: Session = fastapi.Depends(get_db)):
-    data, mode = data.data, data.mode
-    print(data, mode)    
-    if table not in models.TABLES:
-        return {"detail": f"{table} is not in schema", "data": None}
+# @app.post("/api/v2/read/{table}")
+# async def read(table: str, data: schema.READ, db: Session = Depends(get_db)):
+#     data, mode = data.data, data.mode
+#     print(data, mode)
+#     if table not in models.TABLES:
+#         return {"detail": f"{table} is not in schema", "data": None}
 
+#     _data = data.copy()
+#     _keys = None
+#     for i in models.KEYS:
+#         _table = list(i.keys())[0]
+#         if _table == table:
+#             _keys = i[_table]
+#             break
+#     for _key in list(_data.keys()):
+#         if _key not in _keys:
+#             _data.pop(_key)
+#     rtn = crud_v2.read(db, table, _data, mode=mode.upper())
+    
+#     return {"status": 200, "message": f"REQ => Success to read {table}", "data": rtn}
+
+@app.post("/api/v2/read/{table}")
+async def read(request: Request, db: Session = Depends(get_db)):
+    if request.path_params['table'] not in models.TABLES:
+        return {"detail": f"{request.path_params['table']} is not in schema", "data": None}
+    
+    table = request.path_params['table']
+    formData = await request.form()
+    data = loads(formData['data'])
+    mode = formData['mode']
     _data = data.copy()
     _keys = None
+
     for i in models.KEYS:
         _table = list(i.keys())[0]
         if _table == table:
@@ -59,9 +88,14 @@ async def read(table: str, data: schema.READ, db: Session = fastapi.Depends(get_
     for _key in list(_data.keys()):
         if _key not in _keys:
             _data.pop(_key)
+            
     rtn = crud_v2.read(db, table, _data, mode=mode.upper())
     
     return {"status": 200, "message": f"REQ => Success to read {table}", "data": rtn}
+
+@app.get("/client")
+async def client(request: Request):
+    return Jinja2Templates(directory="client").TemplateResponse("client_v2.html", context={"request": request})
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8000, app=app)
